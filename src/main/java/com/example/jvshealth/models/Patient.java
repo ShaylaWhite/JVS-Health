7b2b4c7af49ed371f304729c33f4a1ab27e62fbe
unknown <merrill.huang@gmail.com>
2023-09-27 16:01:35 -0500

    add fields for Patient model

--- src/main/java/com/example/jvshealth/models/Patient.java
+++ src/main/java/com/example/jvshealth/models/Patient.java
@@ -2,8 +2,15 @@ package com.example.jvshealth.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "patients")
 public class Patient {
+
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
 }
