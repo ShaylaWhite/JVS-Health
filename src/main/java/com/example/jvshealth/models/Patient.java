2d1a39421175caf41a470e5492b03f9db60b985f
unknown <merrill.huang@gmail.com>
2023-09-27 16:02:17 -0500

    add annotations for Patient's fields

--- src/main/java/com/example/jvshealth/models/Patient.java
+++ src/main/java/com/example/jvshealth/models/Patient.java
@@ -1,16 +1,20 @@
 package com.example.jvshealth.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "patients")
 public class Patient {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
+    @Column
     private LocalDate birthDate;
 }
