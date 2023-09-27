5c429373f61032b1e83925add6c40f8119366ce2
unknown <merrill.huang@gmail.com>
2023-09-27 16:03:31 -0500

    generate getters, setters, toString, constructors for Patient

--- src/main/java/com/example/jvshealth/models/Patient.java
+++ src/main/java/com/example/jvshealth/models/Patient.java
@@ -17,4 +17,46 @@ public class Patient {
 
     @Column
     private LocalDate birthDate;
+
+    public Patient() {
+    }
+
+    public Patient(Long id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
