a88b6aea8aeba781d7de8596b2e3f5624ba765a9
unknown <merrill.huang@gmail.com>
2023-09-27 15:56:07 -0500

    add Doctor's fields

--- src/main/java/com/example/jvshealth/models/Doctor.java
+++ src/main/java/com/example/jvshealth/models/Doctor.java
@@ -1,4 +1,16 @@
 package com.example.jvshealth.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
 public class Doctor {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
 }
