3ece6a761716baac32d6aac37390cb1877523b96
unknown <merrill.huang@gmail.com>
2023-09-27 15:59:08 -0500

    generate getters, setters, toString, constructors for Doctor

--- src/main/java/com/example/jvshealth/models/Doctor.java
+++ src/main/java/com/example/jvshealth/models/Doctor.java
@@ -17,4 +17,45 @@ public class Doctor {
     @Column
     private String lastName;
 
+    public Doctor() {
+    }
+
+    public Doctor(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
