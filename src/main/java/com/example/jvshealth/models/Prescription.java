04b650bb08575363ee32de4dca86fada01b1845e
unknown <merrill.huang@gmail.com>
2023-09-27 16:39:40 -0500

    add annotations to Prescription to connect to Doctor and Patient models

--- src/main/java/com/example/jvshealth/models/Prescription.java
+++ src/main/java/com/example/jvshealth/models/Prescription.java
@@ -1,16 +1,22 @@
 package com.example.jvshealth.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "prescriptions")
 public class Prescription {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
     private Doctor doctor;
 
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
     private Patient patient;
 
     private String details;
