3a794bbb6632208970869a8197206dc585198bbb
unknown <merrill.huang@gmail.com>
2023-09-27 15:57:17 -0500

    add annotations for Doctor's fields

--- src/main/java/com/example/jvshealth/models/Doctor.java
+++ src/main/java/com/example/jvshealth/models/Doctor.java
@@ -1,16 +1,20 @@
 package com.example.jvshealth.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "doctors")
 public class Doctor {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String firstName;
 
+    @Column
     private String lastName;
 
 }
