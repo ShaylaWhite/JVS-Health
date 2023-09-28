81d86b28d0eedaf1528c8974b51e8c6090ee4329
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:35:07 -0400

    setDoctorDetailService method added to JwtRequestFilter

--- src/main/java/com/example/jvshealth/security/JwtRequestFilter.java
+++ src/main/java/com/example/jvshealth/security/JwtRequestFilter.java
@@ -1,6 +1,7 @@
 package com.example.jvshealth.security;
 
 import com.example.jvshealth.service.DoctorDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,4 +12,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
     private DoctorDetailService doctorDetailService;
     private JWTUtils jwtUtils;
+
+    @Autowired
+    public void setDoctorDetailService(DoctorDetailService doctorDetailService) {
+        this.doctorDetailService = doctorDetailService;
+    }
 }
