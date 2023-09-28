8773358b2090dc6a0e6a5035c3d408be064ed548
Shayla <shayladwhite1@gmail.com>
2023-09-28 10:46:41 -0700

    created hashmap object for the doccontroller

--- src/main/java/com/example/jvshealth/controller/DoctorController.java
+++ src/main/java/com/example/jvshealth/controller/DoctorController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
-@RequestMapping(path = "/auth/doctors/")
+@RequestMapping(path = "/auth/doctors/") //http:localhost
 public class DoctorController {
     @Autowired
     private DoctorService doctorService;
-
+static HashMap<String, Object> result = new HashMap<>();
 }
