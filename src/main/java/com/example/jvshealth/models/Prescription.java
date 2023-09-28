78a7b70839c08948604ce69a649c33332e89c900
unknown <merrill.huang@gmail.com>
2023-09-28 09:36:23 -0500

    add getters, setters, constructors, toString for Prescription model

--- src/main/java/com/example/jvshealth/models/Prescription.java
+++ src/main/java/com/example/jvshealth/models/Prescription.java
@@ -19,5 +19,58 @@ public class Prescription {
     @JoinColumn(name = "patient_id")
     private Patient patient;
 
+    @Column
     private String details;
+
+    public Prescription() {
+    }
+
+    public Prescription(Long id, Doctor doctor, Patient patient, String details) {
+        this.id = id;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.details = details;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Prescription{" +
+                "id=" + id +
+                ", doctor=" + doctor +
+                ", patient=" + patient +
+                ", details='" + details + '\'' +
+                '}';
+    }
 }
