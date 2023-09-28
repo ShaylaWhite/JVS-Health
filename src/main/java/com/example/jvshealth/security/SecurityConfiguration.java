2ce470a84dc7b2e233e2d9361ccdfabd9301e611
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:28:37 -0400

    JavaDoc added for Beans in Security Configuration

--- src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
+++ src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
@@ -9,11 +9,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    /**
+     Defines a bean for the BCryptPasswordEncoder, which is used for hashing passwords.
+     *
+     @return BCryptPasswordEncoder bean
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    /**
+     Defines a bean for the JwtRequestFilter, responsible for handling JWT-based authentication.
+     *
+     @return JwtRequestFilter bean
+     */
     @Bean
     public JwtRequestFilter authJwtRequestFilter() {
         return new JwtRequestFilter();
