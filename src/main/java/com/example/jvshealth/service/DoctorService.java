a65ca8286b8b02ddb8ef9e3092b92704ef6d9816
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:48:58 -0400

    contructor all args created in DoctorService

--- src/main/java/com/example/jvshealth/service/DoctorService.java
+++ src/main/java/com/example/jvshealth/service/DoctorService.java
@@ -2,18 +2,32 @@ package com.example.jvshealth.service;
 
 import com.example.jvshealth.models.Doctor;
 import com.example.jvshealth.repository.DoctorRepository;
+import com.example.jvshealth.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorService {
-    DoctorRepository doctorRepository;
-@Autowired
-    public void setDoctorRepository(DoctorRepository doctorRepository) {
-        this.doctorRepository = doctorRepository;
-    }
+    private final  DoctorRepository doctorRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
     public Doctor findUserByEmailAddress(String emailAddress){
         return doctorRepository.findUserByEmailAddress(emailAddress);
     }
+
+    @Autowired
+    public DoctorService(DoctorRepository DoctorRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils,
+                       @Lazy AuthenticationManager authenticationManager) {
+        this.doctorRepository = doctorRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
+
 }
