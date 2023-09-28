69df5d4ec2f861b9a0513aeb1c428fab618982d4
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:59:59 -0400

    Logger added to JWTUtils

--- src/main/java/com/example/jvshealth/security/JWTUtils.java
+++ src/main/java/com/example/jvshealth/security/JWTUtils.java
@@ -2,7 +2,9 @@ package com.example.jvshealth.security;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class JWTUtils {
-
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
 }
