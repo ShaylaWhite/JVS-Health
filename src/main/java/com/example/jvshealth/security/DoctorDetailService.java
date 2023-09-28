f98a5c41ce9fa12571e5f70ffd7f654e9bdbfeaa
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:41:55 -0400

    Connected DoctorService and DoctorRespository via methods and fields

--- src/main/java/com/example/jvshealth/security/DoctorDetailService.java
+++ src/main/java/com/example/jvshealth/security/DoctorDetailService.java
@@ -1,8 +1,16 @@
 package com.example.jvshealth.security;
 
+import com.example.jvshealth.models.Doctor;
+import com.example.jvshealth.service.DoctorService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Doctor doctor = DoctorService.findUserByEmailAddress;
+    }
 }
--- src/main/java/com/example/jvshealth/service/DoctorService.java
+++ src/main/java/com/example/jvshealth/service/DoctorService.java
@@ -1,7 +1,19 @@
 package com.example.jvshealth.service;
 
+import com.example.jvshealth.models.Doctor;
+import com.example.jvshealth.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoctorService {
+    DoctorRepository doctorRepository;
+@Autowired
+    public void setDoctorRepository(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public Doctor findUserByEmailAddress(String emailAddress){
+        return doctorRepository.findUserByEmailAddress(emailAddress);
+    }
 }
