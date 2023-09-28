b11a046e0c616ec67b8c87b4cc4784bb919c7027
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:50:24 -0400

    Bean authenticationManager added to SecurityConfiguration

--- src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
+++ src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.jvshealth.security;
 import net.bytebuddy.asm.Advice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -52,4 +54,8 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
