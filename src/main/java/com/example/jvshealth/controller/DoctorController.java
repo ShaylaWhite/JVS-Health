4478b7c7a30228d5495a500d4d1935ff230a8a27
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:12:04 -0400

    Controller package created, DoctorController class created

---
+++ src/main/java/com/example/jvshealth/controller/DoctorController.java
@@ -0,0 +1,9 @@
+package com.example.jvshealth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/doctors/")
+public class DoctorController {
+}
