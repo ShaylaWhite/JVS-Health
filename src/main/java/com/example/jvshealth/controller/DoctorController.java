802931d542be858b270c65a7d0732c33291a881c
Shayla <shayladwhite1@gmail.com>
2023-09-28 10:43:42 -0700

    added autowire jpa

--- src/main/java/com/example/jvshealth/controller/DoctorController.java
+++ src/main/java/com/example/jvshealth/controller/DoctorController.java
@@ -1,11 +1,14 @@
 package com.example.jvshealth.controller;
 
 import com.example.jvshealth.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/auth/doctors/")
 public class DoctorController {
+    @Autowired
     private DoctorService doctorService;
+
 }
