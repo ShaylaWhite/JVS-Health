f09ff1f5db0f0ea8d83259f8236a2e930e4354e1
Shayla <shayladwhite1@gmail.com>
2023-09-28 11:28:09 -0700

    created the getters/setters/constrcutor for the loginrequest class

--- src/main/java/com/example/jvshealth/request/LoginRequest.java
+++ src/main/java/com/example/jvshealth/request/LoginRequest.java
@@ -1,4 +1,23 @@
 package com.example.jvshealth.request;
-
 public class LoginRequest {
+    private String emailAddress;
+    private String password;
+
+    // Constructors, getters, and setters
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
