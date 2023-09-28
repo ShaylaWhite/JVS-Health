2a4f070295a79445c2e4dd5cd91d4a7ee4f57d09
Shayla <shayladwhite1@gmail.com>
2023-09-28 11:01:21 -0700

    updated imports fr the doccontroller

--- src/main/java/com/example/jvshealth/controller/DoctorController.java
+++ src/main/java/com/example/jvshealth/controller/DoctorController.java
@@ -1,10 +1,16 @@
 package com.example.jvshealth.controller;
 
+
 import com.example.jvshealth.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 import java.util.HashMap;
 
@@ -21,9 +27,9 @@ public class DoctorController {
 static HashMap<String, Object> result = new HashMap<>();
 static HashMap<String, Object> message = new HashMap<>();
 
-@PostMapping(path = "register/")
+//CREATE USER
 
 
-@PostMapping(path = "login/")
+    @PostMapping(path = "login/")
     <ResponseEnitity>
 }
