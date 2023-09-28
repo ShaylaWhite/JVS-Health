6ec900e4b31a89c56241c5f0bebdb9b2e87203ee
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:30:37 -0400

    SecurityFilterChain bean added to SecurityConfiguration class

--- src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
+++ src/main/java/com/example/jvshealth/security/SecurityConfiguration.java
@@ -4,7 +4,11 @@ import net.bytebuddy.asm.Advice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -26,5 +30,25 @@ public class SecurityConfiguration {
     @Bean
     public JwtRequestFilter authJwtRequestFilter() {
         return new JwtRequestFilter();
+    }
+
+    /**
+     Configures the security filters for handling HTTP requests and defines security rules.
+     *
+     @param http HttpSecurity object for configuring security
+     @return SecurityFilterChain with configured security rules
+     @throws Exception If an error occurs while configuring security
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
