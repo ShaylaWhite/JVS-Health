eb73caf22f001efb322dab38a0b7e02edbcfcdd4
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 13:00:47 -0400

    instance fields added to JWTUtils

--- src/main/java/com/example/jvshealth/security/JWTUtils.java
+++ src/main/java/com/example/jvshealth/security/JWTUtils.java
@@ -1,5 +1,6 @@
 package com.example.jvshealth.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -7,4 +8,11 @@ import java.util.logging.Logger;
 @Service
 public class JWTUtils {
     Logger logger = Logger.getLogger(JWTUtils.class.getName());
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+
 }
