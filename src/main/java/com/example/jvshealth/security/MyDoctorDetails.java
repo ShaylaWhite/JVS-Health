615566e73a80ec9979148681caae01231e4bebb5
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:56:14 -0400

    Doctor instance field added to MyDoctorDetails

--- src/main/java/com/example/jvshealth/security/MyDoctorDetails.java
+++ src/main/java/com/example/jvshealth/security/MyDoctorDetails.java
@@ -1,6 +1,10 @@
 package com.example.jvshealth.security;
 
+import com.example.jvshealth.models.Doctor;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class MyDoctorDetails implements UserDetails {
+
+    private final Doctor doctor;
+
 }
