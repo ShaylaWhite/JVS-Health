549dba877fb9fa533a6eb322830fea3060aaa7a3
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:06:14 -0400

    Doctor interface extended with JPARepository

--- src/main/java/com/example/jvshealth/repository/DoctorRepository.java
+++ src/main/java/com/example/jvshealth/repository/DoctorRepository.java
@@ -1,4 +1,8 @@
 package com.example.jvshealth.repository;
 
-public interface DoctorRepository {
+import com.example.jvshealth.models.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
 }
