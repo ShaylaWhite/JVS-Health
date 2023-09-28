91dbba04c9051a74b0ab5ab9abd7b040eb4cadf9
avrubio <ariadna.vanegas@gmail.com>
2023-09-28 12:52:32 -0400

    parseJwt method added to JwtRequestFilter

--- src/main/java/com/example/jvshealth/security/JwtRequestFilter.java
+++ src/main/java/com/example/jvshealth/security/JwtRequestFilter.java
@@ -2,8 +2,10 @@ package com.example.jvshealth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -22,5 +24,14 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         this.jwtUtils = jwtUtils;
     }
 
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        // Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXJlc2gyQGdhLmNvbSIsImlhdCI6MTY5NDgwMDAzNiwiZXhwIjoxNjk0ODg2NDM2fQ.z3smvkvDJqOYz7699UjvH5JQ51MuWL-KXffegc1UxWU
+        if (StringUtils.hasLength(headerAuth) && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7);
+        }
+        logger.info("No header");
+        return null;
+    }
 
 }
