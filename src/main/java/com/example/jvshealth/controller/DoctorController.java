e21345d793b27efe478211dce696a7b9f008b2b0
unknown <merrill.huang@gmail.com>
2023-09-29 11:00:51 -0500

    started implementing getAllPatients

--- src/main/java/com/example/jvshealth/controller/DoctorController.java
+++ src/main/java/com/example/jvshealth/controller/DoctorController.java
@@ -1,11 +1,15 @@
 package com.example.jvshealth.controller;
 
+import com.example.jvshealth.models.Patient;
 import com.example.jvshealth.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/doctors/")
@@ -27,4 +31,10 @@ public class DoctorController {
 
     static HashMap<String, Object> result = new HashMap<>();
     static HashMap<String, Object> message = new HashMap<>();
+
+    @GetMapping(path = "/{doctorId}/patients/")
+    public ResponseEntity<?> getAllPatients() {
+        List<Patient> patientList = doctorService.getAllPatients();
+        }
+    }
 }
