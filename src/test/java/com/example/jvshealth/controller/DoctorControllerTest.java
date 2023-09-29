ebc1b07cc7fd0f08c46c0d6b69ee01f0f859f2ba
unknown <merrill.huang@gmail.com>
2023-09-29 09:40:57 -0500

    create DoctorControllerTest

---
+++ src/test/java/com/example/jvshealth/controller/DoctorControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.jvshealth.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DoctorControllerTest {
+
+    @Test
+    public void createDoctor() {
+    }
+
+    @Test
+    public void loginUser() {
+    }
+}
