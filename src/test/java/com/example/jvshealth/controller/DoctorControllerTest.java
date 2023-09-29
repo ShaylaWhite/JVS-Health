8a3ac3f49a66b13d7b610ee87e5bb535126e740f
unknown <merrill.huang@gmail.com>
2023-09-29 09:42:32 -0500

    add fields to DoctorController test

--- src/test/java/com/example/jvshealth/controller/DoctorControllerTest.java
+++ src/test/java/com/example/jvshealth/controller/DoctorControllerTest.java
@@ -1,10 +1,22 @@
 package com.example.jvshealth.controller;
 
+import com.example.jvshealth.service.DoctorService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 
+@WebMvcTest(DoctorController.class)
 public class DoctorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DoctorService doctorService;
 
     @Test
     public void createDoctor() {
