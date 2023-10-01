1fe8568c8f87393d0b74f113afd95574c2d83745
avrubio <ariadna.vanegas@gmail.com>
2023-10-01 12:02:04 -0400

    findByPatientIdAndDetails method added to PrescriptionRepository

--- src/main/java/com/example/jvshealth/repository/PrescriptionRepository.java
+++ src/main/java/com/example/jvshealth/repository/PrescriptionRepository.java
@@ -4,6 +4,9 @@ import com.example.jvshealth.models.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+    Optional<Prescription> findByPatientIdAndDetails(Long patientId, String details);
 }
